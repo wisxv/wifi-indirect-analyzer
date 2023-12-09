8daf0ec601af14d6f306022c9ae423074808d99d
wisxv <147903385+wisxv@users.noreply.github.com>
2023-12-09 17:04:19 +0300

    Update channel_hopping.sh

--- channel_hopping.sh
+++ channel_hopping.sh
@@ -7,14 +7,14 @@ channels="1 2 3 4 5 6 7 8 9 10 11 12 13"
 
 sudo systemctl stop NetworkManager wpa_supplicant.service networking.service &&
 sudo ip link set "${device}" down &&
-iw dev "${device}" set monitor control &&
+sudo iw dev "${device}" set monitor control &&
 sudo rfkill unblock wifi &&
 sudo ip link set "${device}" up &&
 
 while true; do
 	for channel in ${channels}; do
     		echo "Setting channel ${channel}, ${width}"
-    		iw "dev ${device}" set channel "${channel}" "${width}"
+    		sudo iw dev "${device}" set channel "${channel}" "${width}"
     		sleep 0.1
 	done
 done
